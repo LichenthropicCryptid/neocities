2acb078526cf43a598f429143437c4d7816f6d5b
LichenthropicCryptid <lichenthropiccryptid@proton.me>
2026-09-15 04:03:44 -0700

    toggle hell

--- public/freezeframe.js
+++ public/freezeframe.js
@@ -114,5 +114,11 @@ document.addEventListener("readystatechange", function () {
     for(const el of document.getElementsByClassName('toggle-gif')) {
       el.addEventListener('click', () => f.toggle());
     }
+     for(const el of document.getElementsByClassName('left-toggle')) {
+      el.addEventListener('click', () => f.toggle());
+    }
+     for(const el of document.getElementsByClassName('right-toggle')) {
+      el.addEventListener('click', () => f.toggle());
+    }
   }
 });
