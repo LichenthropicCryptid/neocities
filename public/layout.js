ae4f2011e5a4ca7e21766a0357e9b899db8b7999
LichenthropicCryptid <lichenthropiccryptid@proton.me>
2026-09-17 14:51:12 -0700

    gf

--- public/layout.js
+++ public/layout.js
@@ -23,28 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-            document.querySelectorAll('.draggable').forEach(el => {
-              let isDragging = false;
-              let offsetX, offsetY;
-  
-              el.addEventListener('mousedown', (e) => {
-                isDragging = true;
-                offsetX = e.clientX - el.offsetLeft;
-                offsetY = e.clientY - el.offsetTop;
-                el.style.cursor = 'grabbing';
-              });
-  
-              document.addEventListener('mousemove', (e) => {
-                if (!isDragging) return;
-                el.style.left = (e.clientX - offsetX) + 'px';
-                el.style.top = (e.clientY - offsetY) + 'px';
-              });
-  
-              document.addEventListener('mouseup', () => {
-                isDragging = false;
-                el.style.cursor = 'grab';
-              });
-            });
+            
             
             
 
