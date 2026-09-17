e7553dcc4bc4e25b456e09dc3cb07ef537b25d98
LichenthropicCryptid <lichenthropiccryptid@proton.me>
2026-09-17 02:10:43 -0700

    jhgf

--- public/layout.js
+++ public/layout.js
@@ -97,6 +97,7 @@ const headerE1 = `
                     <div class="access-fairy">
                         <img src="https://file.garden/anb5fb-RvmNCRWUD/misc/tumblr_inline_o14jlnyNGa1tjuw11_500.gif" title="accessability fairy" alt="accessability menu" width="100px">
                     </div>
+                    <br/>
                     </summary>
                     <div class="accessability">
                     ACCESSABILITY
