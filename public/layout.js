da7c03113ddcf2943948769876c17e0fd9475bbc
LichenthropicCryptid <lichenthropiccryptid@proton.me>
2026-09-17 03:01:15 -0700

    ghf

--- public/layout.js
+++ public/layout.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     initActiveLinks();
   }
-  
+
 
             document.querySelectorAll('.draggable').forEach(el => {
               let isDragging = false;
@@ -39,6 +39,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 isDragging = false;
                 el.style.cursor = 'grab';
               });
+            }
+
             
   // add your own javascript code here...
 
@@ -176,4 +178,4 @@ const sidebarEl1 =
 // Remove all the content inside the `` if you don't have a sidebar.
 const sidebarEl2 = 
         `
-`;})
+`;
