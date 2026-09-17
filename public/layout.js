6f254ea0d09531be3bcd05e2c002727004ff6150
LichenthropicCryptid <lichenthropiccryptid@proton.me>
2026-09-17 01:23:45 -0700

    beep

--- public/layout.js
+++ public/layout.js
@@ -95,9 +95,9 @@ const headerE1 = `
 		<details class="hidden">
                     <summary>
                     <div class="accessability">
-                    ---
-                    ---
-                    ---
+                    -<br/>
+                    -<br/>
+                    -<br/>
                     </div>
                     </summary>
                     <br/>
