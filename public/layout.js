d97512d24a4286e5535ca5b129b47ca452af2c81
LichenthropicCryptid <lichenthropiccryptid@proton.me>
2026-09-17 02:02:33 -0700

    serdtg

--- public/layout.js
+++ public/layout.js
@@ -129,9 +129,10 @@ const headerE1 = `
                     </div>
                     </div class="box">
                     </details>
-          </div>          
+          </div>
+          </div>        
 	    <div id="header" style="height: 150px;">
-		</div>
+		
             <div id="headerArea">
             
                     <nav id="navbar" style="margin-bottom: 10px; margin-top: 10px;">
