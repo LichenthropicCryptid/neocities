3b9411d26f8221d19667299ac20b55a590039b22
LichenthropicCryptid <lichenthropiccryptid@proton.me>
2026-09-16 17:54:35 -0700

    beep

--- public/layout.js
+++ public/layout.js
@@ -100,9 +100,8 @@ const headerE1 = `
                     </summary>
                 
                     <div id="theme-box">
-                        <button class="play-gif">Play GIFs</button>
-                        <button class="stop-gif">Stop GIFs</button>
-                        <button class="toggle-gif">Toggle GIFs</button>
+                    <button id="play-gif" type="button">Play GIFs</button>
+                    <button id="stop-gif" type="button">Stop GIFs</button>
                         <button class="colortheme" onclick="darkmode(), location.reload()">DARK</button>
                         <button class="colortheme" onclick="lightmode(), location.reload()">LIGHT</button>
                     </div>
