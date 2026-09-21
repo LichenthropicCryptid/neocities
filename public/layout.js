5e37ef00068809bbed146608626c49c950298bdd
LichenthropicCryptid <lichenthropiccryptid@proton.me>
2026-09-21 00:21:46 -0700

    beep

--- public/layout.js
+++ public/layout.js
@@ -98,7 +98,7 @@ const headerE1 = `
 		<details class="draggable">
                     <summary>
                     <div>
-                        <img id="access-fairy" src="https://file.garden/anb5fb-RvmNCRWUD/misc/tumblr_inline_o14jlnyNGa1tjuw11_500.gif" title="accessability fairy" alt="accessability menu" width="100px">
+                        <img id="access-fairy" src="https://lichenthropic.neocities.org/images/misc/tumblr_inline_o14jlnyNGa1tjuw11_500.gif" title="accessability fairy" alt="accessability menu" width="100px">
                     </div>
                     <br/>
                     </summary>
