bbe48ab44e24f44d5b54d432920eec4e2c93bf6b
LichenthropicCryptid <lichenthropiccryptid@proton.me>
2026-09-23 03:54:07 -0700

    trythis

--- public/pages/components/layout2.js
+++ public/pages/components/layout2.js
@@ -1,8 +1,8 @@
 async function loadComponent(id, file) {
-    const response = await fetch(file
+    const response = await fetch(file)
     const text = await response.text(
     
-    document.getElementById(id).innerHTML = text;
+    document.getElementById(id).innerHTML = text)
     }
     
     document.addEventListener("DOMContentLoaded", async () => {
@@ -12,8 +12,10 @@ async function loadComponent(id, file) {
     loadComponent("footer", "/components/footer.html"),
     loadComponent("l-sidebar", "/components/leftsidebar2.html"),
     loadComponent("r-sidebar", "/components/rightsidebar2.html")
-    ]
+    ],
     
     document.dispatchEvent(new Event("componentsLoaded")
-    }
-    
+    ,
+)
+)
+})
